7facee442c842371f74eb3944df4e90a0e59cef8
deathmaz <00maz1987@gmail.com>
2023-02-13 12:14:49 +0200

    remove-orphans

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone --depth 1 https://github.com/neovim/neovim.git \
-  && docker-compose up \
-  && docker-compose down \
+  && docker-compose up --remove-orphans \
+  && docker-compose down --remove-orphans \
   && rm -rf $HOME/execs/neovim && cp -r dist $HOME/execs/neovim \
