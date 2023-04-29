84bb49f0206278c31b54eb53c2f8925ea6e53928
deathmaz <00maz1987@gmail.com>
2023-04-29 11:46:35 +0300

    fix: create dif it doesn't exist

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 git clone --depth 1 https://github.com/neovim/neovim.git \
   && docker-compose up --remove-orphans \
   && docker-compose down --remove-orphans \
+  && mkdir -p $HOME/execs \
   && rm -rf $HOME/execs/neovim && cp -r dist $HOME/execs/neovim \
